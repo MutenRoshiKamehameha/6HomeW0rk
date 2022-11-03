bf1dbe54284cd4ccd71321e0493a1602b74200a4
baygeldi <akylbekuulubaygeldi@gmail.com>
2022-11-03 14:35:50 +0600

    asaas

--- 6HomeWork/main.swift
+++ 6HomeWork/main.swift
@@ -7,5 +7,61 @@
 
 import Foundation
 
-print("Hello, World!")
+var randomNumber = Int.random(in:  100000000...999999999)
+
+class User {
+    
+    var name:String
+    var surname:String
+   
+    var code = 0
+    
+    init(name:String, surname:String) {
+    self.name = name
+    self.surname = surname
+    }
+        func numberInfo(){
+        print("Напишите название или код страны")
+        let number = readLine()!
+        switch number{
+            case "\(Country.russia)":
+            code = Country.russia.rawValue
+            
+            case "\(Country.ukraine)":
+            code = Country.ukraine.rawValue
+            
+            case "\(Country.kyrgystan)":
+            code = Country.kyrgystan.rawValue
+            
+            
+        case "\(Country.russia.rawValue)":
+        code = Country.russia.rawValue
+        
+        case "\(Country.ukraine.rawValue)":
+        code = Country.ukraine.rawValue
+        
+        case "\(Country.kyrgystan.rawValue)":
+        code = Country.kyrgystan.rawValue
+        
+        default:
+            code = 996
+        }
+            print("\(name) \(surname) +(\(code))\(randomNumber)")
+        }
+}
+
+
+enum Country:Int{
+   
+    case russia = 7
+    case kyrgystan = 996
+    case uzbekistan = 998
+    case ukraine = 380
+}
+
+var user1 = User(name: "Baygeldi", surname: "Akylbek uulu")
+user1.numberInfo()
+
+
+
 
